9cbd86b0dfeac925f6b2a53a62fec235816b5503
sapph42 <31635854+sapph42@users.noreply.github.com>
2026-09-20 13:37:51 -0400

    Model cleanup

--- Remediator/Classes/Rollback/LogBatch.cs
+++ Remediator/Classes/Rollback/LogBatch.cs
@@ -1,125 +1,128 @@
-﻿using SapphTools.DHA.Stig.Common.Classes;
-using SapphTools.DHA.Stig.Common.Interfaces;
-using System;
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.ObjectModel;
 using System.Diagnostics.CodeAnalysis;
 
 namespace SapphTools.DHA.Stig.Remediator.Classes.Rollback; 
 internal class LogBatch {
+    internal readonly Guid Batch;
+    private readonly LogMachineCollection _machines;
+    public ReadOnlyDictionary <string, LogMachine> MachineLogs => _machines.MachineLogs;
+    public LogBatch(RemediationAction action) {
+        Batch = action.RemediationBatch;
+        _machines = new(action);
+    }
+    public void Add(RemediationAction action) {
+        if (action.RemediationBatch != Batch) {
+            throw new ArgumentException("Provided value is not a proper child of this Batch instance");
+        }
+        _machines.Add(action);
+    }
 }
-internal class LogBatchHost {
-
-}
-internal class LogRule : IEnumerable {
-    internal readonly RemediationAction Source;
-    private readonly List<LogSettingCollection> _settingCols = [];
-    public string RuleId => Source.RuleId;
-    public LogSettingCollection this[int index] {
-        get => _settingCols[index];
+internal class LogMachineCollection : IEnumerable {
+    internal readonly Guid Batch;
+    private readonly Dictionary<string, LogMachine> _machines = new(StringComparer.OrdinalIgnoreCase);
+    public ReadOnlyDictionary<string, LogMachine> MachineLogs => _machines.AsReadOnly();
+    public LogMachine this[string computerName] => _machines[computerName];
+    public int Count => _machines.Count;
+    public LogMachineCollection(RemediationAction action) {
+        Batch = action.RemediationBatch;
+        Add(action);
     }
-    public int Count => _settingCols.Count;
-    public bool IsReadOnly => false;
-    public LogRule(RemediationAction action) {
-        Source = action;
-        Add(new LogAction(action));
-    }
-    public void Add(LogAction action) {
-        if (action.ComputerName != Source.ComputerName ||
-                action.RuleId != Source.RuleId ||
-                action.RemediationBatch != Source.RemediationBatch) {
-            throw new ArgumentException("Provided value is not a proper child of this Index instance");
+    public void Add(RemediationAction action) {
+        if (action.RemediationBatch != Batch) {
+            throw new ArgumentException("Provided value is not a proper child of this Batch instance");
         }
-        if (_settingCols.Where(a => a.ContainsKey(action.SettingIndex)).FirstOrDefault() is LogSettingCollection col) {
-            col.Add(action);
+        if (_machines.TryGetValue(action.ComputerName, out LogMachine? value)) {
+            value.Add(action);
         } else {
-            _settingCols.Add(new(action));
+            _machines.Add(action.ComputerName, new(action));
         }
     }
-    public void Add(LogSetting setting) {
-        if (setting.Source.ComputerName != Source.ComputerName ||
-                setting.Source.RuleId != Source.RuleId ||
-                setting.Source.RemediationBatch != Source.RemediationBatch) {
-            throw new ArgumentException("Provided value is not a proper child of this Index instance");
-        }
-        if (_settingCols.Where(a => a.ContainsValue(setting)).FirstOrDefault() is LogSettingCollection col) {
-            col.Add(setting);
-        } else {
-            _settingCols.Add(new(setting));
-        }
-    }
-    public void Clear() {
-        _settingCols.Clear();
-    }
-    public List<LogSettingCollection>.Enumerator GetEnumerator() => _settingCols.GetEnumerator();
-    public int IndexOf(LogSettingCollection item) => _settingCols.IndexOf(item);
-    public int IndexOf(LogSetting item) {
-        if (_settingCols.Where(a => a.ContainsValue(item)).FirstOrDefault() is LogSettingCollection col) {
-            return _settingCols.IndexOf(col);
-        } else {
-            return -1;
-        }
-    }
-    public void Insert(int index, LogSetting item) {
-        if (item.Source.ComputerName != Source.ComputerName ||
-                item.Source.RuleId != Source.RuleId ||
-                item.Source.RemediationBatch != Source.RemediationBatch) {
-            throw new ArgumentException("Provided value is not a proper child of this Index instance");
-        }
-        if (_settingCols.Where(a => a.ContainsKey(item.SettingIndex)).FirstOrDefault() is LogSettingCollection col &&
-            ) {
-            col.Add(item);
-        } else {
-            _settingCols.Add(new(item));
-        }
-        _settingCols.Insert(index, item);
-    }
-    public bool Remove(LogAction item) => _settingCols.Remove(item);
-    public void RemoveAt(int index) => _settingCols.RemoveAt(index);
+    public bool ContainsKey(string computerName) => _machines.ContainsKey(computerName);
+    public bool TryGetValue(string computerName, out LogMachine? machine) => _machines.TryGetValue(computerName, out machine);
+    public Dictionary<string, LogMachine>.ValueCollection.Enumerator GetEnumerator() => _machines.Values.GetEnumerator();
     IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+}
+internal class LogMachine(RemediationAction action) {
+    internal readonly RemediationAction Source = action;
+    private readonly LogRuleCollection _rules = new(action);
+    public ReadOnlyDictionary<string, LogRule> RuleLogs => _rules.RuleCollection;
+    public string ComputerName => Source.ComputerName;
+
+    public void Add(RemediationAction action) {
+        if (!StringComparer.OrdinalIgnoreCase.Equals(action.ComputerName, Source.ComputerName) ||
+                action.RemediationBatch != Source.RemediationBatch) {
+            throw new ArgumentException("Provided value is not a proper child of this Machine instance");
+        }
+        _rules.Add(action);
+    }
+}
+internal class LogRuleCollection : IEnumerable {
+    internal readonly RemediationAction Source;
+    private readonly Dictionary<string, LogRule> _rules = new(StringComparer.OrdinalIgnoreCase);
+    public ReadOnlyDictionary<string, LogRule> RuleCollection => _rules.AsReadOnly();
+    public LogRule this[string ruleId] => _rules[ruleId];
+    public int Count => _rules.Count;
+    public LogRuleCollection(RemediationAction action) {
+        Source = action;
+        Add(action);
+    }
+    public void Add(RemediationAction action) {
+        if (!StringComparer.OrdinalIgnoreCase.Equals(action.ComputerName, Source.ComputerName) ||
+                action.RemediationBatch != Source.RemediationBatch) {
+            throw new ArgumentException("Provided value is not a proper child of this Machine instance");
+        }
+        if (_rules.TryGetValue(action.RuleId, out LogRule? value)) {
+            value.Add(action);
+        } else {
+            _rules.Add(action.RuleId, new(action));
+        }
+    }
+    public bool ContainsKey(string ruleId) => _rules.ContainsKey(ruleId);
+    public bool TryGetValue(string ruleId, out LogRule? rule) => _rules.TryGetValue(ruleId, out rule);
+    public Dictionary<string, LogRule>.ValueCollection.Enumerator GetEnumerator() => _rules.Values.GetEnumerator();
+    IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+}
+internal class LogRule(RemediationAction action) {
+    internal readonly RemediationAction Source = action;
+    private readonly LogSettingCollection _settings = new(action);
+    public ReadOnlyDictionary<int, LogSetting> SettingLogs => _settings.SettingCollection;
+    public string RuleId => Source.RuleId;
+
+    public void Add(RemediationAction action) {
+        if (action.RuleId != Source.RuleId ||
+                action.ComputerName != Source.ComputerName ||
+                action.RemediationBatch != Source.RemediationBatch) {
+            throw new ArgumentException("Provided value is not a proper child of this Rule instance");
+        }
+        _settings.Add(action);
+    }
 }
 internal class LogSettingCollection : IEnumerable {
     internal readonly RemediationAction Source;
-    private Dictionary<int, LogSetting> _indicies = [];
-
-    public IEqualityComparer<int> Comparer => _indicies.Comparer;
-    public int Count => _indicies.Count;
-    public bool IsReadOnly => false;
-    public LogSetting this[int index] => _indicies[index];
-    public Dictionary<int, LogSetting>.KeyCollection Keys => _indicies.Keys;
-    public Dictionary<int, LogSetting>.ValueCollection Values => _indicies.Values;
-
-    public LogSettingCollection(RemediationAction source) {
-        Source = source;
-        Add(new LogAction(source));
+    private readonly Dictionary<int, LogSetting> _settings = [];
+    public ReadOnlyDictionary<int, LogSetting> SettingCollection => _settings.AsReadOnly();
+    public LogSetting this[int index] => _settings[index];
+    public int Count => _settings.Count;
+    public LogSettingCollection(RemediationAction action) {
+        Source = action;
+        Add(action);
     }
-    public LogSettingCollection(LogSetting setting) {
-        Source = setting.Source;
-        Add(setting);
-    }
-    public void Add(LogSetting item) {
-        if (_indicies.TryGetValue(item.SettingIndex, out LogSetting? value)) {
-            value.Add(item);
-        } else {
-            _indicies.Add(item.SettingIndex, item);
+    public void Add(RemediationAction action) {
+        if (!StringComparer.OrdinalIgnoreCase.Equals(action.RuleId, Source.RuleId) ||
+                !StringComparer.OrdinalIgnoreCase.Equals(action.ComputerName, Source.ComputerName) ||
+                action.RemediationBatch != Source.RemediationBatch) {
+            throw new ArgumentException("Provided value is not a proper child of this Rule instance");
         }
-    }
-    public void Add(LogAction action) {
-        if (_indicies.TryGetValue(action.SettingIndex, out LogSetting? value)) {
+        if (_settings.TryGetValue(action.SettingIndex, out LogSetting? value)) {
             value.Add(action);
         } else {
-            _indicies.Add(action.SettingIndex, new(action));
+            _settings.Add(action.SettingIndex, new(action));
         }
     }
-    public void Clear() => _indicies.Clear();
-    public bool ContainsKey(int settingIndex) => _indicies.ContainsKey(settingIndex);
-    public bool ContainsValue(LogSetting index) => _indicies.ContainsValue(index);
-    public int EnsureCapacity(int capacity) => _indicies.EnsureCapacity(capacity);
-    public Dictionary<int, LogSetting>.Enumerator GetEnumerator() => _indicies.GetEnumerator();
-    public bool Remove(int settingIndex) => _indicies.Remove(settingIndex);
-    public bool Remove(LogSetting index) => _indicies.Remove(index.SettingIndex);
-    public bool Remove(LogAction action) => _indicies[action.SettingIndex].Remove(action);
-    public bool Remove(int settingIndex, out LogSetting? index) => _indicies.Remove(settingIndex, out index);
+    public bool ContainsKey(int settingIndex) => _settings.ContainsKey(settingIndex);
+    public bool ContainsValue(LogSetting index) => _settings.ContainsValue(index);
+    public Dictionary<int, LogSetting>.ValueCollection.Enumerator GetEnumerator() => _settings.Values.GetEnumerator();
     IEnumerator IEnumerable.GetEnumerator() {
         return GetEnumerator();
     }
@@ -128,64 +131,28 @@ internal class LogSetting : IEnumerable {
     internal readonly RemediationAction Source;
     private readonly List<LogAction> _actions = [];
     public int SettingIndex => Source.SettingIndex;
-    public LogAction this[int index] { 
-        get => _actions[index];
-        set {
-            if (value.SettingIndex != Source.SettingIndex ||
-                    value.ComputerName != Source.ComputerName ||
-                    value.RuleId != Source.RuleId ||
-                    value.RemediationBatch != Source.RemediationBatch) {
-                throw new ArgumentException("Provided value is not a proper child of this Index instance");
-            }
-            _actions[index] = value;
-        }
-    }
+    public LogAction this[int index] => _actions[index];
     public int Count => _actions.Count;
-    public bool IsReadOnly => false;
     public LogSetting(RemediationAction action) {
         Source = action;
         Add(action);
     }
-    public void Add(LogAction action) {
+    public void Add(RemediationAction action) {
         if (action.SettingIndex != Source.SettingIndex ||
-                action.ComputerName != Source.ComputerName ||
-                action.RuleId != Source.RuleId ||
+                !StringComparer.OrdinalIgnoreCase.Equals(action.RuleId, Source.RuleId) ||
+                !StringComparer.OrdinalIgnoreCase.Equals(action.ComputerName, Source.ComputerName) ||
                 action.RemediationBatch != Source.RemediationBatch) {
-            throw new ArgumentException("Provided value is not a proper child of this Index instance");
+            throw new ArgumentException("Provided value is not a proper child of this Setting instance");
         }
         _actions.Add(action);
     }
-    public void Add(LogSetting action) {
-        if (action.Source.SettingIndex != Source.SettingIndex ||
-                action.Source.ComputerName != Source.ComputerName ||
-                action.Source.RuleId != Source.RuleId ||
-                action.Source.RemediationBatch != Source.RemediationBatch) {
-            throw new ArgumentException("Provided value is not a proper child of this Index instance");
-        }
-        _actions.Add(action.Source);
-    }
-    public void Clear() {
-        _actions.Clear();
-    }
     public bool Contains(LogAction item) => _actions.Contains(item);
-    public void CopyTo(LogAction[] array, int arrayIndex) => _actions.CopyTo(array, arrayIndex);
     public IEnumerator<LogAction> GetEnumerator() => _actions.GetEnumerator();
-    public int IndexOf(LogAction item) => _actions.IndexOf(item);
-    public void Insert(int index, LogAction item) {
-        if (item.SettingIndex != Source.SettingIndex ||
-                item.ComputerName != Source.ComputerName ||
-                item.RuleId != Source.RuleId ||
-                item.RemediationBatch != Source.RemediationBatch) {
-            throw new ArgumentException("Provided value is not a proper child of this Index instance");
-        }
-        _actions.Insert(index, item);
-    }
-    public bool Remove(LogAction item) => _actions.Remove(item);
-    public void RemoveAt(int index) => _actions.RemoveAt(index);
     IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
 }
-internal class LogAction {
-    private readonly RemediationAction _source;
+[method: SetsRequiredMembers]
+internal class LogAction(RemediationAction action) {
+    private readonly RemediationAction _source = action;
     internal Guid RemediationBatch => _source.RemediationBatch;
     internal string RuleId => _source.RuleId;
     internal string? Description => _source.Description;
@@ -200,24 +167,5 @@ internal class LogAction {
     public string? FailureMessage => _source.FailureMessage;
     public DateTimeOffset RemediationTimestamp => _source.RemediationTimestamp;
 
-    [SetsRequiredMembers]
-    public LogAction(RemediationAction action) {
-        _source = action;
-    }
     public static implicit operator LogAction(RemediationAction action) => new(action);
-    public static implicit operator RemediationAction(LogAction action) => new() {
-        RemediationBatch = action.RemediationBatch,
-        RuleId = action.RuleId,
-        Description = action.Description,
-        ComputerName = action.ComputerName,
-        SettingIndex = action.SettingIndex,
-        TargetType = action.TargetType,
-        Target = action.Target,
-        RollbackCapability = action.RollbackCapability,
-        Before = action.Before,
-        After = action.After,
-        Result = action.Result,
-        FailureMessage = action.FailureMessage,
-        RemediationTimestamp = action.RemediationTimestamp
-    };
 }
