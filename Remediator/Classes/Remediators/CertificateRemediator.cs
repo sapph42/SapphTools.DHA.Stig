8f065485699de6a047d946d6c119644f737c1990
sapph42 <31635854+sapph42@users.noreply.github.com>
2026-09-21 13:31:10 -0400

    Implementation added

--- Remediator/Classes/Remediators/CertificateRemediator.cs
+++ Remediator/Classes/Remediators/CertificateRemediator.cs
@@ -1,12 +1,73 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace SapphTools.DHA.Stig.Remediator.Classes.Remediators;
+﻿namespace SapphTools.DHA.Stig.Remediator.Classes.Remediators;
 internal class CertificateRemediator : IRemediator {
     public static void Remediate(Rule rule, int settingIndex, Guid batch, string? computerName, bool whatIf = true) {
-        throw new NotImplementedException();
+        if (!rule.Settings.Where(s => s.Order == settingIndex).Any()) {
+            throw new ArgumentException($"No such setting exists at order index {settingIndex}");
+        }
+        Setting setting = rule.Settings.Where(s => s.Order == settingIndex).First();
+        IValue data = setting.Data;
+        if (data is not CertificatesValue val) {
+            throw new ArgumentException($"Expected {nameof(rule)}.Settings[{nameof(settingIndex)}].Data to be of type CertificatesValue, was {data.GetType().Name}");
+        }
+        string targetHost = computerName ?? Environment.MachineName;
+        RemediationPreAction pre = new() {
+            RemediationBatch = batch,
+            RuleId = rule.RuleId,
+            Description = rule.Description,
+            ComputerName = targetHost,
+            SettingIndex = settingIndex
+        };
+        foreach (ArtifactCatalog artifact in val.Artifacts.Cast<ArtifactCatalog>()) {
+            if (!artifact.Verify()) {
+                Logger.LogError(
+                    pre,
+                    TargetType.CertStore,
+                    val.Target,
+                    $"Artifact {artifact.FullPath} failed validation",
+                    whatIf
+                );
+            } else {
+                if (whatIf) {
+                    Logger.LogWhatIf(
+                        pre,
+                        TargetType.CertStore,
+                        val.Target,
+                        RollbackCapability.NotApplicable,
+                        null,
+                        val
+                    );
+                    return;
+                }
+                CryptoWinApi.AddCerts(targetHost, val.Target, artifact.FullPath, out int success, out int noAction, out int failed);
+                if (success == 0 && failed == 0) {
+                    Logger.LogNoAction(
+                        pre,
+                        TargetType.CertStore,
+                        val.Target,
+                        val
+                    );
+                    return;
+                }
+                if (failed == 0) {
+                    Logger.LogSuccess(
+                        pre,
+                        TargetType.CertStore,
+                        val.Target,
+                        RollbackCapability.NotApplicable,
+                        null,
+                        val
+                    );
+                    return;
+                }
+                Logger.LogError(
+                    pre,
+                    TargetType.CertStore,
+                    val.Target,
+                    $"{success} certificates were added, {failed} certificates failed to add, {noAction} certificates were already present",
+                    whatIf
+                );
+            }
+        }
+        
     }
 }
